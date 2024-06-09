eb9dab69697826c2e01948320d31fbc6f6d09270
Deepak-technical <deepakakash156@gmail.com>
2024-06-09 20:09:09 +0530

    code added

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import { StreamVideoProvider } from '@/providers/StreamClientProvider'
+import React, { ReactNode } from 'react'
+
+const RootLayout = ({children}:{children:ReactNode}) => {
+  return (
+    <StreamVideoProvider>{children}</StreamVideoProvider>
+  )
+}
+
+export default RootLayout
